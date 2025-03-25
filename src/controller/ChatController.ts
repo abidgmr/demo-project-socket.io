645fba5051bcca279f60e460ab6169f04fca5a5e
Abid Hussain <abid.hussain@gmrwebteam.com>
2025-03-25 17:25:40 +0530

    Some modification in Chat controller

--- src/controller/ChatController.ts
+++ src/controller/ChatController.ts
@@ -10,6 +10,7 @@ import {
 } from "inversify-express-utils";
 import { TYPES } from "../config/types";
 import IChatService from "../services/interface/IChatService";
+import { MessageDto } from "../dtos/ChatDto";
 
 @controller("/chat")
 export class ChatController implements interfaces.Controller {
@@ -20,15 +21,34 @@ export class ChatController implements interfaces.Controller {
   }
 
   @httpPost("/send")
-  public async send(@request() req: Request, @response() res: Response): Promise<boolean | void> {
-    
-    const { socketId, message } = req.body;
-    const response = this._chatService.sendToSocket(socketId, message);
-    res.status(201).json(response);
+  public async send(
+    @request() req: Request,
+    @response() res: Response
+  ): Promise<Response<MessageDto>> {
+    try {
+      const { userId, socketId, message } = req.body;
+      const response = await this._chatService.sendToSocket(
+        userId,
+        socketId,
+        message
+      );
+      console.log("response", response);
+
+      return res.status(201).json(response);
+    } catch (error) {
+      console.error("Error sending message:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Internal Server Error",
+      });
+    }
   }
 
   @httpPost("/broadcast")
-  public async broadcast(@request() req: Request, @response() res: Response): Promise<boolean | void> {
+  public async broadcast(
+    @request() req: Request,
+    @response() res: Response
+  ): Promise<boolean | void> {
     const { message } = req.body;
     const response = this._chatService.broadcastMessage(message);
     res.status(201).json(response);
--- src/services/interface/IChatService.ts
+++ src/services/interface/IChatService.ts
@@ -2,6 +2,6 @@ import ChatDto from "../../dtos/ChatDto";
 import Response from "../../dtos/Response";
 
 export default interface IChatService {
-  sendToSocket(socketId: string, message: string): Promise<Response<ChatDto>>;
+  sendToSocket( userId: number, socketId: string, message: string): Promise<Response<ChatDto>>;
   broadcastMessage(message: string): Promise<Response<ChatDto>>;
 }
